37433e2c6f98be8b137a290d1d2ea0d813eb5ad1
brianrizqi <rizqibrian27@gmail.com>
2019-12-18 23:17:36 +0700

    add confirm

--- resources/views/create_division.blade.php
+++ resources/views/create_division.blade.php
@@ -3,34 +3,54 @@
     <div class="section-header">
         <h1>Tambah Divisi</h1>
         <div class="section-header-breadcrumb">
-{{--            <div class="breadcrumb-item active"><a href="/">Dashboard</a></div>--}}
-{{--            <div class="breadcrumb-item active"><a href="/">Plant</a></div>--}}
-{{--            <div class="breadcrumb-item">Create</div>--}}
+            {{--            <div class="breadcrumb-item active"><a href="/">Dashboard</a></div>--}}
+            {{--            <div class="breadcrumb-item active"><a href="/">Plant</a></div>--}}
+            {{--            <div class="breadcrumb-item">Create</div>--}}
         </div>
     </div>
     <div class="section-body">
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <form method="POST" action="{{route('division.store')}}">
+                    <form>
                         <div class="card-body">
                             <div class="form-group row mb-4">
                                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama Divisi</label>
                                 <div class="col-sm-12 col-md-7">
-                                    <input type="text" class="form-control" name="name">
+                                    <input type="text" class="form-control" id="name" name="name">
                                 </div>
                             </div>
                             <div class="form-group row mb-4">
                                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                                 <div class="col-sm-12 col-md-7">
-                                    <button class="btn btn-primary" type="submit">Simpan</button>
+                                    <button class="btn btn-primary"
+                                            data-confirm="Hapus?|Apakah data akan disimpan?"
+                                            data-confirm-yes="simpan()">
+                                        Simpan
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                        @csrf
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        function simpan() {
+            var name = $('#name').val();
+            $.ajax({
+                type: 'POST',
+                url: '{{ route('division.store')}}',
+                data: {
+                    'name': name,
+                    '_token': '{{ csrf_token() }}',
+                },
+                success: function () {
+                    window.location.href = '{{ route('division') }}';
+                    return false;
+                }
+            })
+        }
+    </script>
 @endsection
